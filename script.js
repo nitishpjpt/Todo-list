fa090b9e1121ab3bca735660addef0a8511e1f1f
nitishpjpt <116824310+nitishpjpt@users.noreply.github.com>
2023-06-27 19:17:06 +0530

    JavaScript file

---
+++ script.js
@@ -0,0 +1,68 @@
+//pehla sara variable langa//
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+
+//Array of object //
+const tasks = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
+
+showAllTasks();
+function showAllTasks() {
+  tasks.forEach((value, index) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "task");
+
+    const innerDiv = document.createElement("div");
+    div.append(innerDiv);
+
+    const p = document.createElement("p");
+    p.innerText = value.title;
+    innerDiv.append(p);
+
+    const span = document.createElement("span");
+    span.innerText = value.description;
+    innerDiv.append(span);
+
+    const btn = document.createElement("button");
+    btn.setAttribute("class", "deleteBtn");
+
+    btn.innerText = "-";
+
+    //eventlistner for deleted word//
+    btn.addEventListener("click", () => {
+      removeTasks();
+      tasks.splice();
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      showAllTasks();
+    });
+
+    div.append(btn);
+    container.append(div);
+  });
+}
+
+function removeTasks() {
+  tasks.forEach(() => {
+    const div = document.querySelector(".task");
+    div.remove();
+  });
+}
+
+//add to envent listner in button e.preventDeault rokti
+// ha refersh hona sa button submit click hona par
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  removeTasks();
+  tasks.push({
+    title: title.value,
+    description: description.value,
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log(tasks);
+  showAllTasks();
+});
